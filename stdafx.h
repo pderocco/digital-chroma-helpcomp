1fe38d51335a0c60ad1e98007909661e580f758a
Paul D. DeRocco <info@digitalchroma.com>
2019-11-05 15:15:51 -0800

    Initial version

---
+++ stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <SDKDDKVer.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+#include <string_view>
+#include <windows.h>
+
+using namespace std;
+using namespace std::experimental::filesystem::v1;
